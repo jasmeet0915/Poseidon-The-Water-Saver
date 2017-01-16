d7796b757568513ad9a502fb5115474e88f9e30c
Jasmeet Singh <jasmeet0915@gmail.com>
2017-01-16 23:16:16 +0530

    Update sender_code.ino
    comments added

--- sender_code.ino
+++ sender_code.ino
@@ -1,7 +1,9 @@
 #include <SoftwareSerial.h>
-SoftwareSerial myserial(2, 3);
+SoftwareSerial myserial(2, 3); //RX and TX of HC-12 module
 int trigPin = 5;
 int echoPin = 6;
+int tank_height; //place the height of the tank here 
+int tankTop_level; //distance of the water in tank from the ultrasonic sensor after which you want to turn of the motor 
 long duration;
 int distance;
 void setup() {
@@ -23,11 +25,11 @@ void loop() {
   Serial.println("distance:");
   Serial.print(distance);
   delay(1000);
-  if(distance==10){
+  if(distance == tank_height){ // send '3' to receiver end when the tank gets empty
     myserial.println("3");
     Serial.println("message sent");
   }
-  if(distance==5){
+  if(distance == tankTop_level){ // send '1' when the water reaches topmost level in tank
     myserial.println("1");
     Serial.println("message sent");
   }
